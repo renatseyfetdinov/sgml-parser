a4bb9637fca24c24f77579247c89fa619e9676ee
Renat <renat@agilesolutions.az>
2019-06-23 21:17:51 +0400

    initial

---
+++ Sgml parser/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Sgml_parser.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+        private readonly SgmlConverter converter;
+
+        public HomeController(ILogger<HomeController> logger, SgmlConverter converter)
+        {
+            this.logger = logger;
+            this.converter = converter;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [DisableRequestSizeLimit]
+        public async Task<IActionResult> Index(IFormFile inputFile)
+        {
+            try
+            {
+                string[] attachmemtTags = { "TEXT" };
+                return Content(converter.ConvertToJson(inputFile.OpenReadStream(), attachmemtTags)) ;
+            }
+            catch (Exception exc)
+            {
+                logger.LogError(exc, "Error");
+                return Content("Error on converting. See more details in log");
+            }
+
+
+        }
+
+
+    }
+}
---
+++ Sgml parser/SgmlConverter.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Sgml_parser
+{
+    public class SgmlConverter
+    {
+        private readonly ILogger<SgmlConverter> logger;
+
+        public SgmlConverter(ILogger<SgmlConverter> logger)
+        {
+            this.logger = logger;
+        }
+
+        public string ConvertToJson(Stream inputStream, string[] attachmemtTags)
+        {
+            using (TextReader reader = new StreamReader(inputStream))
+            {
+                return JsonFromXml(reader, attachmemtTags);
+            }
+        }
+
+        public string ConvertToJson(string input, string[] attachmemtTags)
+        {
+            using (TextReader reader = new StringReader(input))
+            {
+                return JsonFromXml(reader, attachmemtTags);
+            }
+        }
+
+        private string ConvertBinaryToBase64(TextReader reader, string[] attachmemtTags)
+        {
+            string value = reader.ReadToEnd();
+            foreach(string item in attachmemtTags)
+            {
+                value = Regex.Replace(value, @"<TEXT>(?>.*?</TEXT>)", m =>
+                {
+                    logger.LogTrace("Binary content converted to base64");
+                    string valueToReplace = m.Groups[0].Value;
+                    valueToReplace = valueToReplace.Substring(("<" + item + ">").Length);
+                    valueToReplace = valueToReplace.Substring(0, valueToReplace.Length - ("</" + item + ">").Length);
+                    var buffer = Encoding.UTF8.GetBytes(valueToReplace);
+                    return "<"+ item + ">" + Convert.ToBase64String(buffer) + "</" + item + ">";
+                }, RegexOptions.Singleline);
+            }
+
+            return value;
+        }
+
+        private string JsonFromXml(TextReader reader, string[] attachmemtTags)
+        {
+            string preProcessed = ConvertBinaryToBase64(reader, attachmemtTags);
+            using (TextReader readerAfter = new StringReader(preProcessed))
+            {
+                XmlDocument doc = XmlFromSgml(readerAfter);
+                string json = JsonConvert.SerializeXmlNode(doc);
+                return json;
+            }
+        }
+
+
+        private XmlDocument XmlFromSgml(TextReader textReader)
+        {
+            Stack<string> stack = new Stack<string>();
+            TextWriter logWriter = new StringWriter();
+            try
+            {
+                Sgml.SgmlReader reader = new Sgml.SgmlReader();
+                reader.DocType = "HTML";
+                reader.WhitespaceHandling = WhitespaceHandling.All;
+                reader.CaseFolding = Sgml.CaseFolding.ToLower;
+                reader.InputStream = textReader;
+                reader.ErrorLog = logWriter;
+
+                StringWriter writer = new StringWriter();
+                XmlWriter xmlWriter = null;
+
+                try
+                {
+                    xmlWriter = new XmlTextWriter(writer);
+
+                    bool rootElement = true;
+                    string lastElementName = "";
+                    while ((reader.Read()) && !reader.EOF)
+                    {
+                        switch (reader.NodeType)
+                        {
+                            case XmlNodeType.Element:
+                                try
+                                {
+                                    xmlWriter.WriteStartElement(reader.LocalName);
+                                    xmlWriter.WriteAttributes(reader, true);
+                                    if (reader.IsEmptyElement)
+                                    {
+                                        xmlWriter.WriteEndElement();
+                                        lastElementName = "";
+                                    }
+                                    else
+                                    {
+                                        lastElementName = reader.LocalName;
+                                    }
+                                }
+                                catch (Exception exc)
+                                {
+                                    logger.LogError(exc, lastElementName);
+                                }
+
+
+                                break;
+
+                            case XmlNodeType.Text:
+                                if (string.IsNullOrEmpty(lastElementName))
+                                {
+                                    string text = reader.Value;
+                                    xmlWriter.WriteString(text);
+                                }
+                                else
+                                {
+                                    if (rootElement)
+                                    {
+                                        rootElement = false;
+                                        stack.Push(".");
+                                    }
+                                    else
+                                    {
+                                        xmlWriter.WriteElementString("value", reader.Value);
+                                        xmlWriter.WriteEndElement();
+                                        stack.Push(lastElementName);
+                                    }
+                                    lastElementName = "";
+                                }
+                                break;
+
+                            case XmlNodeType.EndElement:
+                                if (stack.Peek() == reader.LocalName)
+                                {
+                                    stack.Pop();
+                                    break;
+                                }
+
+                                logger.LogTrace(reader.LocalName);
+                                try
+                                {
+                                    xmlWriter.WriteEndElement();
+                                    lastElementName = "";
+                                }
+                                catch (Exception exc)
+                                {
+                                    logger.LogError(exc, lastElementName);
+                                }
+                                break;
+
+
+                        }
+                    }
+                }
+                finally
+                {
+                    if (xmlWriter != null)
+                    {
+                        xmlWriter.Close();
+                    }
+                }
+
+
+                string xml = writer.ToString();
+
+                XmlDocument doc = new XmlDocument();
+
+                doc.PreserveWhitespace = true;
+
+                doc.LoadXml(xml);
+                return doc;
+            }
+            finally
+            {
+                string str = logWriter.ToString();
+                if (string.IsNullOrEmpty(str))
+                    logger.LogError(str);
+                logWriter.Dispose();
+            }
+        }
+    }
+}
